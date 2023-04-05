4863203049ce21435319351646e125fc44be7f03
StormDragon_64 <ashtonbettis@gmail.com>
2023-04-05 15:30:07 -0700

    -Apparently create has a brass gearbox texture already. All that time wasted texturing for nothing, I guess. -Attempt to fix custom tab -Fix there being no vertical gearbox

--- src/main/java/net/stormdragon_64/groups/ModGroup.java
+++ src/main/java/net/stormdragon_64/groups/ModGroup.java
@@ -3,13 +3,13 @@ package net.stormdragon_64.groups;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.stormdragon_64.block.ModBlocks;
-import org.jetbrains.annotations.NotNull;
+import net.stormdragon_64.create_plus.CreatePlus;
 
 public class ModGroup extends CreativeModeTab {
     public static ModGroup MAIN_TAB;
 
     public ModGroup(String name) {
-        super("Create++");
+        super(CreatePlus.MOD_ID+":"+name);
         MAIN_TAB = this;
     }
 
--- src/main/java/net/stormdragon_64/item/ModItems.java
+++ src/main/java/net/stormdragon_64/item/ModItems.java
@@ -1,15 +1,22 @@
 package net.stormdragon_64.item;
+import com.simibubi.create.Create;
 import com.simibubi.create.content.contraptions.itemAssembly.SequencedAssemblyItem;
+import com.simibubi.create.content.contraptions.relays.gearbox.VerticalGearboxItem;
+import com.simibubi.create.foundation.data.AssetLookup;
 import com.tterrag.registrate.util.entry.ItemEntry;
 
 import static net.stormdragon_64.create_plus.CreatePlus.REGISTRATE;
 
 public class ModItems {
-
     //The items
     public static final ItemEntry<SequencedAssemblyItem> INCOMPLETE_COGWHEEL = REGISTRATE
             .item("incomplete_cogwheel", SequencedAssemblyItem::new)
             .register();
+
+    public static final ItemEntry<VerticalGearboxItem> VERTICAL_BRASS_GEARBOX =
+            REGISTRATE.item("vertical_brass_gearbox", VerticalGearboxItem::new)
+                    .model(AssetLookup.customBlockItemModel("brass_gearbox", "item_vertical"))
+                    .register();
 
     //Register method - important, don't touch!
     public static void register() {}
