766f9955ac1b11d3ac06cc1f1ac37920433db9cf
StormDragon_64 <ashtonbettis@gmail.com>
2023-04-05 12:27:47 -0700

    Add Brass Gearbox - Should also fix missing textures for brass chain drive Make Incomplete Cogwheel a Sequenced Assembly Item (I think this adds progress bar, maybe?? idk)

--- src/main/java/net/stormdragon_64/block/ModBlocks.java
+++ src/main/java/net/stormdragon_64/block/ModBlocks.java
@@ -1,16 +1,26 @@
 package net.stormdragon_64.block;
+import com.simibubi.create.AllSpriteShifts;
+import com.simibubi.create.Create;
 import com.simibubi.create.content.contraptions.relays.encased.EncasedBeltBlock;
 import com.simibubi.create.content.contraptions.relays.encased.EncasedBeltGenerator;
+import com.simibubi.create.content.contraptions.relays.encased.EncasedCTBehaviour;
+import com.simibubi.create.content.contraptions.relays.gearbox.GearboxBlock;
 import com.simibubi.create.foundation.block.BlockStressDefaults;
+import com.simibubi.create.foundation.data.AssetLookup;
+import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.simibubi.create.foundation.data.SharedProperties;
 import com.tterrag.registrate.util.entry.BlockEntry;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MaterialColor;
+
+import static com.simibubi.create.foundation.data.BlockStateGen.axisBlock;
 import static com.simibubi.create.foundation.data.ModelGen.customItemModel;
 import static com.simibubi.create.foundation.data.TagGen.axeOrPickaxe;
 import static net.stormdragon_64.create_plus.CreatePlus.REGISTRATE;
 
 public class ModBlocks {
-    //stuff to actually register the items
+    //The Items themselves:
+
     public static final BlockEntry<EncasedBeltBlock> BRASS_CHAIN_DRIVE = REGISTRATE
     .block("brass_chain_drive", EncasedBeltBlock::new)
             .initialProperties(SharedProperties::stone)
@@ -23,6 +33,20 @@ public class ModBlocks {
             .transform(customItemModel())
             .register();
 
+    public static final BlockEntry<GearboxBlock> BRASS_GEARBOX = Create.REGISTRATE.block("brass_gearbox", GearboxBlock::new)
+            .initialProperties(SharedProperties::stone)
+            .properties(BlockBehaviour.Properties::noOcclusion)
+            .transform(BlockStressDefaults.setNoImpact())
+            .transform(axeOrPickaxe())
+            .onRegister(CreateRegistrate.connectedTextures(() -> new EncasedCTBehaviour(AllSpriteShifts.BRASS_CASING)))
+            .onRegister(CreateRegistrate.casingConnectivity((block, cc) -> cc.make(block, AllSpriteShifts.BRASS_CASING,
+                    (s, f) -> f.getAxis() == s.getValue(GearboxBlock.AXIS))))
+            .blockstate((c, p) -> axisBlock(c, p, $ -> AssetLookup.partialBaseModel(c, p), true))
+            .item()
+            .transform(customItemModel())
+            .register();
+
+    //Register Method - Don't touch!
     public static void register() {}
 
     }
--- src/main/java/net/stormdragon_64/item/ModItems.java
+++ src/main/java/net/stormdragon_64/item/ModItems.java
@@ -1,12 +1,13 @@
 package net.stormdragon_64.item;
+import com.simibubi.create.content.contraptions.itemAssembly.SequencedAssemblyItem;
 import com.tterrag.registrate.util.entry.ItemEntry;
-import net.minecraft.world.item.Item;
+
 import static net.stormdragon_64.create_plus.CreatePlus.REGISTRATE;
 
 public class ModItems {
     //stuff to actually register the items
-    public static ItemEntry<Item> INCOMPLETE_COGWHEEL = REGISTRATE
-            .item("incomplete_cogwheel", Item::new)
+    public static final ItemEntry<SequencedAssemblyItem>
+            INCOMPLETE_COGWHEEL = REGISTRATE.item("incomplete_cogwheel", SequencedAssemblyItem::new)
             .register();
 
     public static void register() {}
