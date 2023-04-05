7651bcaead13d9e08d942b7019ebe87815b1f74e
StormDragon_64 <ashtonbettis@gmail.com>
2023-04-05 10:39:03 -0700

    remove unnecessary imports

--- src/main/java/net/stormdragon_64/block/ModBlocks.java
+++ src/main/java/net/stormdragon_64/block/ModBlocks.java
@@ -1,16 +1,10 @@
 package net.stormdragon_64.block;
-
-import com.simibubi.create.Create;
 import com.simibubi.create.content.contraptions.relays.encased.EncasedBeltBlock;
 import com.simibubi.create.content.contraptions.relays.encased.EncasedBeltGenerator;
 import com.simibubi.create.foundation.block.BlockStressDefaults;
 import com.simibubi.create.foundation.data.SharedProperties;
 import com.tterrag.registrate.util.entry.BlockEntry;
-import com.tterrag.registrate.util.entry.ItemEntry;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MaterialColor;
-
 import static com.simibubi.create.foundation.data.ModelGen.customItemModel;
 import static com.simibubi.create.foundation.data.TagGen.axeOrPickaxe;
 import static net.stormdragon_64.create_plus.CreatePlus.REGISTRATE;
